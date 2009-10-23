9780b49c1c939e4e01146148170308d48272bcc8
Stepan Rakhimov <strakh@macbook.local>
2009-10-23 17:10:08 +0400

    google visualization charts

--- stats/urls.py
+++ stats/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^$', 'stats.views.index', name='stats_list'),
     url(r'^(?P<stats_id>\d+)/$', 'stats.views.index', name='tracker_list_stats_clicked'),
+    url(r'^trend/(?P<trend_id>\d+)/$', 'stats.views.trend', name='stats_trend'),
+    url(r'^pack/(?P<pack_id>\d+)/$', 'stats.views.pack', name='stats_pack'),
 )
--- stats/views.py
+++ stats/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseRedirect
 from tracker.models import Tracker, Trend, Pack, TrendStatistics, TrackerStatistics, PackStatistics, ChannelStatistics, Statistics, ParsedResult
 from datetime import datetime, timedelta
 from scratchpad.models import Scratchpad, Item
+import gviz_api
 
 @login_required
 def index(request, stats_id=None):
@@ -22,7 +23,23 @@ def index(request, stats_id=None):
         if isinstance(context_vars['cur_stats'].owner, TrackerStatistics):
             tracker = context_vars['cur_stats'].owner.tracker
             context_vars['tracker'] = tracker
-            
+
+        google_desc = {
+                       'interval': ('number', 'Interval'),
+                       'daily_change': ('number', 'Daily change')}
+
+        owner = context_vars['cur_stats'].owner
+        google_data = []
+        for stats in owner.stats.all():
+            google_data.append({'interval':stats.interval, 'daily_change':float(stats.daily_change)})
+        data_table = gviz_api.DataTable(google_desc)
+        data_table.LoadData(google_data)
+        context_vars['jscode'] = data_table.ToJSCode("jscode_data",
+                               columns_order=("interval", "daily_change"),
+                               order_by="interval")
+
+
+
         if request.method == 'POST':
             tracker = context_vars['cur_stats'].owner.get_tracker()
             try:
@@ -41,5 +58,67 @@ def index(request, stats_id=None):
             item.author = request.user
             item.save()
             return HttpResponseRedirect(reverse('scratchpad-scratchpad_view',args=[spad.id]))
+
+    return direct_to_template(request, template='stats.html', extra_context=context_vars)
+
+@login_required
+def trend(request, trend_id=None):
+    context_vars = dict()
+    trend_stats = list(TrendStatistics.objects.all())
+    for ts in trend_stats[:]:
+        if ts.trend.muaccount != request.muaccount:
+            trend_stats.remove(ts)    
+    context_vars['trend_stats'] = trend_stats
+    
+    if trend_id:
+        trend_stat = TrendStatistics.objects.get(id=trend_id)
+        context_vars['cur_trend'] = trend_stat
+        tracker_stats = trend_stat.trackerstats.all()
+
+        jscode = 'var data = new google.visualization.DataTable();\n'
+        for tracker_stat in tracker_stats:
+          jscode += 'data.addColumn("number", "%s");\n' % tracker_stat
+
+        jscode += 'data.addRows(7);\n'
+        i=0
+        j=0
+        for tracker_stat in tracker_stats:
+          for stats in tracker_stat.stats.all():
+            jscode += 'data.setValue(%d, %d, %d);\n' % (j, i, stats.total_7days)
+            j += 1
+          i += 1
+          j = 0
+        context_vars['jscode'] = jscode
+
+
+    return direct_to_template(request, template='stats.html', extra_context=context_vars)
+
+@login_required
+def pack(request, pack_id=None):
+    context_vars = dict()
+    trend_stats = list(TrendStatistics.objects.all())
+    for ts in trend_stats[:]:
+        if ts.trend.muaccount != request.muaccount:
+            trend_stats.remove(ts)    
+    context_vars['trend_stats'] = trend_stats
+    
+    if pack_id:
+        pack_stat = PackStatistics.objects.get(id=pack_id)
+        context_vars['cur_pack'] = pack_stat
+        channel_stats = pack_stat.channelstats.all()
+
+        jscode = 'var data = new google.visualization.DataTable();\n'
+        jscode += 'data.addColumn("string", "%s");\n' % 'Channel'
+        jscode += 'data.addColumn("number", "%s");\n' % 'Mentions'
+
+        jscode += 'data.addRows(%d);\n' % len(channel_stats)
+        i=0
+        for channel_stat in channel_stats:
+          stat = channel_stat.stats.get(interval=24)
+          jscode += 'data.setValue(%d, %d, "%s");\n' % (i, 0, channel_stat)
+          jscode += 'data.setValue(%d, %d, %d);\n' % (i, 1, stat.total_7days)
+          i += 1
+        context_vars['jscode'] = jscode
+
 
     return direct_to_template(request, template='stats.html', extra_context=context_vars)
