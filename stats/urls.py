28f3eb3a4bd410f00bcf3e8b6ae34ff93224acaa
Stepan Rakhimov <strakh@macbook.local>
2009-10-30 17:01:50 +0300

    fixed typo

--- stats/urls.py
+++ stats/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^pack/(?P<pack_id>\d+)/$', 'stats.views.pack', name='stats_pack'),
     url(r'^channel/(?P<channel_id>\d+)/$', 'stats.views.channel', name='stats_channel'),
     url(r'^mentions/js/$', 'stats.views.mentions_js', name='stats_mentions_js'),
-    url(r'^mentions/map/$', 'stats.views.mentions_map', name='stats_mentions_map'),
+#    url(r'^mentions/map/$', 'stats.views.mentions_map', name='stats_mentions_map'),
     url(r'^tracker/(?P<tracker_id>\d+)/js/$', 'stats.views.tracker_mentions_js', name='stats_tracker_mentions_js'),
 #    url(r'^tracker/(?P<tracker_id>\d+)/map/$', 'stats.views.tracker_mentions_map', name='stats_tracker_mentions_map'),
 )
