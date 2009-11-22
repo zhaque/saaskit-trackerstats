a25759c8c0ff7cd761f09c7279a2f435d35d7627
CrowdSense <admin@crowdsense.com>
2009-11-22 06:09:06 +0000

    setup files added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+        'setuptools',
+        'Django',
+]
+ 
+setup(name="trackerstats",
+           version="0.1",
+           description="Django application viewing statistics information via Google Visialuzation API",
+           author="CrowdSense",
+           author_email="admin@crowdsense.com",
+           packages=find_packages(),
+           include_package_data=True,
+           install_requires = install_requires,
+           entry_points="""
+           # -*- Entry points: -*-
+           """,
+           dependency_links = ['http://dist.repoze.org',],
+)
